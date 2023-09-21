ee2fafefe546ccd36488f8b0bec1683d9cbc9c6f
200kairiEEZ <144981209+200kairiEEZ@users.noreply.github.com>
2023-09-21 23:54:36 +0000

    ああああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,21 @@
 import streamlit as st
-import random
-st.title("素数")
-if st.button("素数おみくじを引く"):
-    results = ["２","３","５","７","１１","１３","１７","１９","２３","２９",]
-    result = random.choice(results)
-    st.write(f"結果:{result}")
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Streamlitアプリケーションのタイトルを設定
+st.title("自動絵画アプリ")
+
+# 円の中心座標と半径のユーザー入力を受け取る
+center_x = st.number_input("円の中心のX座標", min_value=0, max_value=500, value=250)
+center_y = st.number_input("円の中心のY座標", min_value=0, max_value=500, value=250)
+radius = st.number_input("円の半径", min_value=10, max_value=200, value=50)
+
+# 円を描画する
+fig, ax = plt.subplots()
+circle = plt.Circle((center_x, center_y), radius, fill=False, color='blue', linewidth=2)
+ax.add_patch(circle)
+
+# グラフの表示
+st.pyplot(fig)
+
 
