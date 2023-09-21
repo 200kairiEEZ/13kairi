6d74d9842f68d8c21e41598e786dab2d9a9acb59
200kairiEEZ <144981209+200kairiEEZ@users.noreply.github.com>
2023-09-21 04:44:36 +0000

    ｑｑ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 st.title("素数")
 if st.button("素数おみくじを引く"):
-    results = ["２","３","５","７","１１","１３","１７","１９","","",]
+    results = ["２","３","５","７","１１","１３","１７","１９","２３","２９",]
     result = random.choice(results)
     st.write(f"結果:{result}")
 
